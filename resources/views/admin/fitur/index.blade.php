d13095e938fd0d218ac8ee879dd3b116c930ae00
Abdeepratama <abdeeprataman@gmail.com>
2025-12-08 11:04:00 +0700

    sdikit perbaikan di dashboard fitur

--- resources/views/admin/fitur/index.blade.php
+++ resources/views/admin/fitur/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Semua Fitur Aplikasi</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('landing/img/logo_reang.png') }}">
 
     <!-- Bootstrap & Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
